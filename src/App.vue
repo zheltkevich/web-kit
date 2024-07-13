4b886f3065aceb72dd7ae1aa350ffd2283ac4898
Aleksandr <zheltkevich42@gmail.com>
2024-07-13 15:13:16 +0300

    Update navigation

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import AppHeader from '@/components/app/AppHeader.vue'
 import AppNav from '@/components/app/AppNav.vue'
@@ -6,52 +7,68 @@ import AppMain from '@/components/app/AppMain.vue'
 
 const $router = useRouter()
 const routes = $router.getRoutes()
+const navigationVisible = ref(false)
+
+const changeVisibility = val => {
+    navigationVisible.value = val
+}
 </script>
 
 <template>
     <AppHeader />
-    <AppNav :routes="routes" />
+    <AppNav
+        :class="{ visible: navigationVisible }"
+        :routes="routes"
+        @toggle="changeVisibility" />
     <AppMain />
 </template>
 
 <style lang="scss">
-nav a.router-link-exact-active {
-    color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-    background-color: transparent;
-}
-
-nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-    border: 0;
-}
-
 /* stylelint-disable-next-line selector-max-id */
 #app {
     display: grid;
-    grid-template-rows: auto 1fr;
-    grid-template-columns: auto auto 1fr;
+    grid-template-rows: auto 1fr auto; /* header и main */
+    grid-template-columns: 1fr; /* Навигацию убираем из потока для мобильных */
+    height: 100vh;
+
+    @media (width > 768px) {
+        grid-template-rows: auto 1fr auto;
+        grid-template-columns: auto 1fr;
+    }
 }
 
 .app-header {
     grid-row: 1;
-    grid-column: 1 / span 3;
+    grid-column: 1 / 3;
 }
 
 .app-nav {
-    grid-row: 2;
-    grid-column: 1;
+    position: fixed; /* фиксированное позиционирование для навигации */
+    width: 100%; /* на всю ширину экрана */
+    height: 100%; /* выдвигается на всю высоту экрана */
+    background-color: var(--color-background);
+    transition: transform 0.2s ease-in-out; /* плавное выдвижение */
+    transform: translateY(calc(100% - 60px)); /* изначально скрыта за пределами экрана */
+    inset: 0;
+
+    @media (width > 768px) {
+        position: static;
+        grid-row: 2;
+        grid-column: 1;
+        transform: translateY(0);
+    }
+
+    &.visible {
+        transform: translateY(0); /* отображается на экране */
+    }
 }
 
 .app-main {
-    grid-row: 2;
-    grid-column: 3;
+    grid-column: 1;
+
+    @media (width > 768px) {
+        grid-row: 2;
+        grid-column: 2;
+    }
 }
 </style>
--- src/components/app/AppNav.vue
+++ src/components/app/AppNav.vue
@@ -1,27 +1,83 @@
 <script setup>
+import { ref } from 'vue'
 defineProps({
     routes: {
         type: Array,
         required: true,
     },
 })
+
+const navigationVisible = ref(false)
+const toggle = () => {
+    navigationVisible.value = !navigationVisible.value
+    $emit('toggle', navigationVisible.value)
+}
+const $emit = defineEmits(['toggle'])
 </script>
 
 <template>
-    <nav class="app-nav">
-        <RouterLink
-            v-for="route in routes"
-            :key="route.name"
-            :to="{ name: route.name }">
-            {{ route.name }}
-        </RouterLink>
-    </nav>
+    <aside class="app-nav">
+        <header class="app-nav__header">
+            <h3>navigation</h3>
+            <button
+                class="app-nav__toggle"
+                @click="toggle">
+                {{ navigationVisible ? 'hide' : 'show' }}
+            </button>
+        </header>
+        <nav class="app-nav__list">
+            <RouterLink
+                v-for="route in routes"
+                :key="route.name"
+                :to="{ name: route.name }">
+                {{ route.name }}
+            </RouterLink>
+        </nav>
+    </aside>
 </template>
 
-<style scoped>
+<style lang="scss">
 .app-nav {
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
+
+    &__header {
+        display: flex;
+        justify-content: space-between;
+        padding: 16px;
+    }
+
+    &__list {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 16px;
+    }
+
+    &__toggle {
+        cursor: pointer;
+
+        @media (width > 768px) {
+            display: none;
+        }
+    }
+}
+
+nav a.router-link-exact-active {
+    color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+    background-color: transparent;
+}
+
+nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+    border: 0;
 }
 </style>
